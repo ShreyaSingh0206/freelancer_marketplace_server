f38729f484d10bedd49cb10e69038051ac8b38b9
ShreyaSingh0206 <shreyakittu2003@gmail.com>
2025-12-16 15:34:09 +0530

    change

--- server.js
+++ server.js
@@ -59,8 +59,6 @@ app.use(cors({
 app.use(cookieParser());
 
 
-app.use(bodyParser.json());
-
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
